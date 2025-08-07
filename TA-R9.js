124c7e6379208583b55a7ebbba66f52b679c3459
メイラボ <144579709+MeyMey-lab@users.noreply.github.com>
2025-08-07 11:15:14 +0900

    Update TA-R9.js

--- TA-R9.js
+++ TA-R9.js
@@ -2154,6 +2154,343 @@ var r9Seap = () => {
 	}
 }
 
+class AllocUtils {
+  static simpleStar() {
+    const starUps = Array.from(game.starBonuses).filter((x) => x.id >= 4000 && x.id < 5000 && x.isAvailable);
+    const variables = Array.from(game.variables).filter((x) => x.id > 0 && x.isAvailable);
+
+    starUps.forEach((x) => x.refund(-1));
+
+    const len = Math.min(starUps.length, variables.length);
+
+    let doubleUps = new Set(Array(len).keys());
+    let singleUps = new Set();
+
+    const dThreshold = 0.00001; // 0.001%
+    const sThreshold = dThreshold / 100;
+    const trivialStars = 0.001 * game.starsTotal;
+    const MAX_ITER = 100;
+
+    for (let k = 0; k < MAX_ITER; k++) {
+      let toMove = [];
+      let toDelete = [];
+
+      let best = null;
+      let best2 = null;
+
+      for (const i of doubleUps) {
+        const up = starUps[i];
+
+        up.buy(-1);
+        const maxLevels = up.level;
+        up.refund(-1);
+
+        const doubleLevels = this.nextDouble(variables[i].level);
+
+        if (maxLevels < doubleLevels) {
+          toMove.push(i);
+          continue;
+        }
+
+        const dumpLevels = maxLevels - this.lastDouble(variables[i].level + maxLevels);
+
+        let cost = up.currency.value;
+        up.buy(dumpLevels);
+        cost -= up.currency.value;
+        let dx = game.x;
+        up.refund(dumpLevels);
+        dx -= game.x;
+
+        if (dx < dThreshold * game.x) {
+          toDelete.push(i);
+          continue;
+        }
+
+        if (best == null || best.dx * cost < dx * best.cost) {
+          best2 = best;
+          best = {
+            isDouble: true,
+            i: i,
+            dx: dx,
+            cost: cost,
+            cnt: dumpLevels,
+          };
+        } else if (best2 == null || best2.dx * cost < dx * best2.cost) {
+          best2 = {
+            isDouble: true,
+            i: i,
+            dx: dx,
+            cost: cost,
+            cnt: dumpLevels,
+          };
+        }
+      }
+
+      toMove.forEach((i) => {
+        doubleUps.delete(i);
+        singleUps.add(i);
+      });
+      toDelete.forEach((i) => {
+        doubleUps.delete(i);
+      });
+      toDelete = [];
+
+      for (const i of singleUps) {
+        const up = starUps[i];
+        const cost = up.cost.getCost(up.level);
+
+        if (cost > up.currency.value) {
+          toDelete.push(i);
+          continue;
+        }
+
+        up.buy(1);
+        let dx = game.x;
+        up.refund(1);
+        dx -= game.x;
+
+        if (dx < sThreshold * game.x) {
+          toDelete.push(i);
+          continue;
+        }
+
+        if (best == null || best.dx * cost < dx * best.cost) {
+          best2 = best;
+          best = {
+            isDouble: false,
+            i: i,
+            dx: dx,
+            cost: cost,
+            cnt: 1,
+          };
+        } else if (best2 == null || best2.dx * cost < dx * best2.cost) {
+          best2 = {
+            isDouble: false,
+            i: i,
+            dx: dx,
+            cost: cost,
+            cnt: 1,
+          };
+        }
+      }
+
+      toDelete.forEach((i) => {
+        singleUps.delete(i);
+      });
+
+      if (best == null) break;
+
+      if (best.isDouble) {
+        starUps[best.i].buy(best.cnt);
+        doubleUps.delete(best.i);
+        singleUps.add(best.i);
+      } else if (best2 == null) {
+        starUps[best.i].buy(-1);
+        singleUps.delete(best.i);
+      } else {
+        const bestup = starUps[best.i];
+        let cost = best.cost;
+        let dx = best.dx;
+        for (let i = 0; i < MAX_ITER; i++) {
+          bestup.buy(1);
+
+          cost = bestup.cost.getCost(bestup.level);
+          if (cost > bestup.currency.value) break;
+          // mitigate edge cases where we have a cheap variable competing with an expensive one.
+          if (cost < trivialStars) continue;
+
+          bestup.buy(1);
+          dx = game.x;
+          bestup.refund(1);
+          dx -= game.x;
+
+          if (best2.dx * cost > dx * best2.cost) break;
+        }
+      }
+    }
+  }
+
+  static nextDouble(level) {
+    if (level >= 24000) return 400 - (level % 400);
+    if (level >= 10000) return 200 - (level % 200);
+    if (level >= 6000) return 100 - (level % 100);
+    if (level >= 1500) return 50 - (level % 50);
+    if (level >= 10) return 25 - (level % 25);
+    return 10 - level;
+  }
+
+  static lastDouble(level) {
+    if (level >= 24000) return level % 400;
+    if (level >= 10000) return level % 200;
+    if (level >= 6000) return level % 100;
+    if (level >= 1500) return level % 50;
+    if (level >= 25) return level % 25;
+    if (level >= 10) return level - 10;
+    return level;
+  }
+
+  static simpleStudent() {
+    // number of purchases to backtrack and brute force; 4 if gradf < ee30k, 10 otherwise
+    // const REFUND_CNT = game.statistics.graduationF < BigNumber.fromComponents(1, 2, 29994) ? 4 : 10;
+
+    const upgrades = Array.from(game.researchUpgrades).filter((x) => x.id <= 101 && x.isAvailable);
+    upgrades.forEach((x) => x.refund(-1));
+
+    if (buyR9.level) game.researchUpgrades[8].buy(-1);
+    else game.researchUpgrades[8].refund(-1);
+
+    const maxLevels = upgrades.map((x) => x.maxLevel);
+    const expIndex = upgrades.length - 1;
+    let levels = upgrades.map((x) => x.level);
+
+    let sigma = game.sigma.toNumber();
+
+    let curSum = BigNumber.ZERO;
+    let history = [];
+
+    // edit in case of emergency
+    const vals = [
+      (game.dt * game.acceleration * (game.isRewardActive ? 1.5 : 1)).log(),
+      (1 + game.t).log() * 0.7,
+      (1 + game.starsTotal).log(),
+      (1 + game.db).log() / (100 * (10 + game.db).log10()).sqrt(),
+      (1 + game.dmu).log() / 1300,
+      ((1 + game.dpsi).log() / 255) * (10 + game.dpsi).log10().sqrt(),
+    ].map((v) => v.toNumber());
+
+    while (true) {
+      let cand = null;
+      let cval = BigNumber.ZERO;
+
+      for (let i = 0; i < upgrades.length; i++) {
+        if (levels[i] >= maxLevels[i]) continue;
+
+        const cost = i == expIndex ? 2 : this.researchCost(levels[i]);
+        const curval = i == expIndex ? curSum / 20 : vals[i] / cost;
+
+        if (curval > cval) {
+          cand = cost <= sigma ? i : null; // flag if best is unreachable.
+          cval = curval;
+        }
+      }
+
+      if (cand == null) break;
+
+      history.push(cand);
+      if (cand == expIndex) {
+        sigma -= 2;
+      } else {
+        curSum += vals[cand];
+        sigma -= this.researchCost(levels[cand]);
+      }
+      levels[cand] += 1;
+    }
+
+    while (history.length > 0) {
+      let pool = 1;
+      let dims = 0;
+
+      for (let i = 0; i < upgrades.length; i++) {
+        if (levels[i] >= maxLevels[i]) continue;
+        let more = i == expIndex ? Math.floor(sigma / 2) : this.maxPurchaseCount(levels[i], sigma);
+        pool *= Math.min(more, maxLevels[i] - levels[i]) + 1;
+        dims += 1;
+      }
+
+      const heur = dims < 6 ? pool / 3 : pool / (dims == 6 ? 20 : 60);
+
+      if (heur > this.MAX_DFS_SIZE) break;
+
+      const lastbest = history.pop();
+
+      if (lastbest == expIndex) {
+        levels[lastbest] -= 1;
+        sigma += 2;
+      } else {
+        const lastlevel = levels[lastbest] - 1;
+        const lastcost = this.researchCost(lastlevel);
+        levels[lastbest] -= 1;
+        sigma += lastcost;
+        curSum -= vals[lastbest];
+      }
+    }
+
+    let search = (i, sigma, curSum) => {
+      // TODO un-reuse variables
+      if (i == expIndex) {
+        const cnt = Math.min((levels[i] + sigma / 2) >> 0, 6);
+        return { cnt: [cnt], maxSum: curSum * (1 + cnt / 10) };
+      }
+      let maxres = null;
+      for (let j = levels[i]; j <= maxLevels[i]; j++) {
+        let res = search(i + 1, sigma, curSum);
+        if (maxres == null || res.maxSum >= maxres.maxSum) {
+          maxres = res;
+          maxres.cnt.push(j);
+        }
+        sigma -= this.researchCost(j);
+        if (sigma < 0) break;
+        curSum += vals[i];
+      }
+      return maxres;
+    };
+
+    const found = search(0, sigma, curSum);
+    for (let i = 0; i <= expIndex; i++) upgrades[i].buy(found.cnt[expIndex - i]);
+  }
+
+  static researchCost(curLevel) {
+    return Math.floor(curLevel / 2 + 1);
+  }
+
+  static maxPurchaseCount(curLevel, sigma) {
+    let levels = 0;
+
+    if (this.researchCost(curLevel) > sigma) return levels;
+
+    if (curLevel % 2 == 1) {
+      sigma -= this.researchCost(curLevel);
+      curLevel += 1;
+      levels += 1;
+    }
+
+    curLevel += 1;
+    const bulks = Math.floor((-curLevel + Math.sqrt(curLevel * curLevel + 4 * sigma)) / 2);
+
+    sigma -= bulks * (curLevel + bulks);
+    curLevel += 2 * bulks - 1;
+    levels += 2 * bulks;
+
+    if (this.researchCost(curLevel) <= sigma) {
+      levels += 1;
+    }
+
+    return levels;
+  }
+
+  static debugSimpleStudentSnapshot() {
+    let output = {};
+    output.sigma = game.sigma.toNumber();
+    output.useR9 = !!buyR9.level;
+    Array.from(game.researchUpgrades).forEach((x) => {
+      output[x.id] = x.level;
+    });
+    return JSON.stringify(output);
+  }
+
+  static debugSimpleStudentPopup(debugTexts) {
+    const output = debugTexts.join("\n");
+    const popup = ui.createPopup({
+      title: "STUDENT ERROR",
+      content: ui.createStackLayout({
+        children: [ui.createLabel({ text: output }), ui.createEntry({ placeholder: output })],
+      }),
+    });
+    popup.show();
+  }
+}
+AllocUtils.MAX_DFS_SIZE = 300;
+
 class UIutils {
 	
 	static createLatexButton(header, variable, id = -1) {
@@ -2281,13 +2618,16 @@ var getUpgradeListDelegate = () => {
 
 	let performR9SeapButton = UIutils.createLatexClickButton("R9スワップを実行", r9Seap);
 	performR9SeapButton.row = 1;
+
+	let performStarOptimizeButton = UIutils.createLatexClickButton("スターを変数に分配", AllocUtils.simpleStar);
+	performStarOptimizeButton.row = 2;
 	
 	let height = ui.screenHeight * 0.055;
 
 	let performTheorySwitchGrid = ui.createGrid({
 		rowSpacing: 3,
 		rowDefinitions: [height, height],
-		children: [performTheorySwitchButton, performR9SeapButton]
+		children: [performTheorySwitchButton, performR9SeapButton, performStarOptimizeButton]
 	})
 	
 	let enableVariablePurchaseButton = UIutils.createLatexButton("変数自動購入", enableVariablePurchase);
